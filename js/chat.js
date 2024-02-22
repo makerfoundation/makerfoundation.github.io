eb3137e44e762089eda98d962530ac7d815f6295
makerfoundation <155825249+makerfoundation@users.noreply.github.com>
2024-02-22 12:33:35 +0900

    Add files via upload

---
+++ js/chat.js
@@ -0,0 +1,22 @@
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = "e518e6efa2f0a7bb1f5edf8003229e174fb01480";
+window.smartsupp ||
+  (function (d) {
+    window.addEventListener("load", (event) => {
+      setTimeout(() => {
+        var s,
+          c,
+          o = (smartsupp = function () {
+            o._.push(arguments);
+          });
+        o._ = [];
+        s = d.getElementsByTagName("script")[0];
+        c = d.createElement("script");
+        c.type = "text/javascript";
+        c.charset = "utf-8";
+        c.async = true;
+        c.src = "https://www.smartsuppchat.com/loader.js?";
+        s.parentNode.insertBefore(c, s);
+      }, 2000);
+    });
+  })(document);
